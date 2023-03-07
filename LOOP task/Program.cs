68cbbaf85846d8dc0d6b19d85322697fb3f0f4e5
sahinmelikov <1rima621melikov@gmail.com>
2023-03-06 22:26:54 -0800

    LOOPtsk

---
+++ LOOP task/Program.cs
@@ -0,0 +1,15 @@
+﻿int en = 8, uzunluq = 12;
+Console.WriteLine("Fiqurun uzunluğunu qeyd edin :");
+uzunluq = int.Parse(Console.ReadLine());
+Console.WriteLine("Fiqurun enin qeyd edin :");
+en = int.Parse(Console.ReadLine());
+for (int I = 1; I <= uzunluq; I++)
+
+{
+    Console.WriteLine();
+    for (int j = 1; j <= uzunluq; j++)
+    {
+        Console.Write(".");
+
+    }
+}
